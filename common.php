b6f05835f2afeac614c49c169a664ae9c4783d6f
leon.zhang <leon.zhang@dhcp-10-32-12-79.dev.gree.jp>
2012-10-23 19:17:03 +0900

    daliy

--- common.php
+++ common.php
@@ -2,26 +2,3 @@
 
  require("../web_main/php/gen_lib.php");
 
- //Language definitions.
- $lang_en=array(
-  "Email",
-  "Twitter",
-  "Webibo",
- );
- $lang_cn=array(
-  "电子邮件",
-  "Twitter",
-  "微博",
- );
- $lang_jp=array(
-  "メール",
-  "ツイッター",
-  "Webibo",
- );
- $langs_array=("en"->$lang_en)
- define("PIDX_CLANG_EMAIL",0);
- define("PIDX_CLANG_TWITTER",1);
- define("PIDX_CLANG_WEIBO",2);
-
- function get
-
--- contact.php
+++ contact.php
@@ -2,12 +2,13 @@
 
  require("common.php");
     
+ $lang=ReadGETnPOSTParam("lang",DEFLANG_NAME);
  $lang_strings = retrieve_lang_strings("");
 
 ?>
 <html>
 <head>
-<title><?php = ?></title>
+<title><?php echo lang_getContent($lang,PIDX_CLANG_CONTACT);?></title>
 </head>
 
 <body>
@@ -18,12 +19,12 @@
 Born: Checgu, Sichuan, China<br>
 Current location: Minato-ku Tokyo, Japan.<br>
 
-<?php echo GetCommonLang(PIDX_CLANG_EMAIL);?>: <a href="emailto:kimryo.ultimate 'at' gmail.com">kimryo.ultimate 'at' gmail.com</a>
-<a herf="https://twitter.com/Leon_Zh" title="<?php echo GetCommonLang(PIDX_CLANG_TWITTER);?>" target="_blank"><?php echo GetCommonLang(PIDX_CLANG_TWITTER);?></a>
+<?php echo lang_getContent($lang,PIDX_CLANG_EMAIL);?>: <a href="mailto:kimryo.ultimate 'at' gmail.com">kimryo.ultimate 'at' gmail.com</a>
+<a herf="https://twitter.com/Leon_Zh" title="<?php echo GetCommonLang(PIDX_CLANG_TWITTER);?>" target="_blank"><?php echo lang_getContent($lang,PIDX_CLANG_TWITTER);?></a>
 <a herf="http://Gplus.to/LeonZhang" title="Google+" target="_blank">Google+</a>
 <a herf="http://jp.linkedin.com/pub/leon-zhang/3a/271/160" title="LinkedIn" target="_blank">LinkedIn</a>
 <a herf="http://www.facebook.com/leon.zhang.313" title="Facebook" target="_blank">Facebook</a>
-<a herf="http://t.qq.com/leon_zhx" title="<?php echo GetCommonLang(PIDX_CLANG_WEIBO);?>" target="_blank"><?php echo GetCommonLang(PIDX_CLANG_WEIBO);?></a>
+<a herf="http://t.qq.com/leon_zhx" title="<?php echo GetCommonLang(PIDX_CLANG_WEIBO);?>" target="_blank"><?php echo lang_getContent($lang,PIDX_CLANG_WEIBO);?></a>
 
 </body>
 </html>
